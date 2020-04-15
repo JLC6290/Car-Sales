15835bcb22766f205870ce54b12487eda282ee95
James <jlc6290@gmail.com>
2020-04-15 11:01:10 -0500

    MVP completed, transfering project form laptop to home machine and pushing to github

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,15 +1,19 @@
 import React from 'react';
-
 import AddedFeature from './AddedFeature';
+import { connect } from 'react-redux';
+import { removeFeature } from '../actions/action';
 
 const AddedFeatures = props => {
+  const removeItem = item => {
+    props.removeFeature(item);
+  }
   return (
     <div className="content">
       <h6>Added features:</h6>
       {props.car.features.length ? (
         <ol type="1">
           {props.car.features.map(item => (
-            <AddedFeature key={item.id} feature={item} />
+            <AddedFeature delete={removeItem} key={item.id} feature={item} />
           ))}
         </ol>
       ) : (
@@ -18,5 +22,9 @@ const AddedFeatures = props => {
     </div>
   );
 };
-
-export default AddedFeatures;
+const mapStateToProps = (state) => {
+  return {
+    car: state.car
+  }
+}
+export default connect(mapStateToProps, {removeFeature})(AddedFeatures);
